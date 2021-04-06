171ae10884bd22256ae8c4fd3ca91200dbefa863
SnakeWiz <pffrdias@gmail.com>
2021-04-06 00:44:58 -0300

    tweak(vendor): include PPlay lib

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import os
+import sys
+
+# Add vendor directory to module search path
+parent_dir = os.path.abspath(os.path.dirname(__file__))
+vendor_dir = os.path.join(parent_dir, 'vendor')
+
+sys.path.append(vendor_dir)
+
 from game import Game
 
 game = Game()
