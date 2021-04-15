c5104740afcc5848e11695199a7392b0b06db708
Pedro Fontoura <pedrofontoura@id.uff.br>
2021-04-15 20:49:38 -0300

    fix: handle PPlay submodule structure

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ import sys
 parent_dir = os.path.abspath(os.path.dirname(__file__))
 vendor_dir = os.path.join(parent_dir, 'vendor')
 
+pplay_dir = os.path.join(vendor_dir, 'pplay')
+
 sys.path.append(vendor_dir)
+
+# Handle PPlay repo structure
+sys.path.append(pplay_dir)
 
 from game import Game
 
